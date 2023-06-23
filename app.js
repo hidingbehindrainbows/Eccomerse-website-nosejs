61f46496c9f7326365f9e03be6534a5962ecc13c
hidingbehindrainbows <117367646+hidingbehindrainbows@users.noreply.github.com>
2023-06-23 10:57:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(3000);
+const partialPath = `${__dirname}` ;
+console.log(partialPath);
+
+app.set(`view engine`, `ejs`);
+app.set(`views`, path.join(__dirname, `views`));
+app.use(express.static(__dirname + '/public'));
+
+console.log(`Running`);
+
+app.get(`/`, (req, res)=>{
+    res.render(`homepage`, {title: "Homepage", partialPath});
+});
+
+app.get(`/about`, (req, res)=>{
+    res.render(`about`, {title: "About Us", partialPath});
+});
+
+app.get(`/shop`, (req, res)=>{
+    res.render(`shop`, {title: "Shopping!!", partialPath});
+});
+
+app.get(`/about/`, (req, res)=>{
+    res.redirect(`/about`)
+});
+
+app.use((req, res)=>{
+    res.render(`404`, {title: "404 Not Found", partialPath});
+});
