431b8edd28f29ce1a34246facb733f8b4325eae6
hidingbehindrainbows <117367646+hidingbehindrainbows@users.noreply.github.com>
2023-07-02 20:21:26 +0530

    Add files via upload

---
+++ functions/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http')
+
+const app = express();
+
+const port = 3000
+// app.listen(3000);
+app.listen(process.env.PORT || port, () => console.log("Listening on PORT"));
+const partialPath = `${__dirname}` ;
+// console.log(partialPath);
+
+app.engine('html', require('ejs').renderFile);
+app.set(`view engine`, `ejs`);
+app.set(`views`, path.join(__dirname, `views`));
+app.use(express.static(__dirname, 'public'));
+
+
+app.get(`/`, (req, res)=>{
+    res.render(`homepage`, {title: "Homepage", partialPath});
+});
+
+app.get(`/about`, (req, res)=>{
+    res.render(`about`, {title: "About Us", partialPath});
+});
+
+app.get(`/shop`, (req, res)=>{
+    res.render(`shop`, {title: "Shopping!!", partialPath});
+});
+
+app.get(`/about/`, (req, res)=>{
+    res.redirect(`/about`)
+});
+
+app.use(('/.netlify/functions/api', router)); 
+// {
+//     res.render(`404`, {title: "404 Not Found", partialPath});
+// });
+module.exports.handler = serverless(app);
